9b2507b6e46ffaccb4996cff4b87521d188fe429
septianwijaya45 <septianwijaya170845@gmail.com>
2022-01-16 02:27:55 +0700

    add model & migration Proyek

---
+++ app/Models/Proyek.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proyek extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'introduction',
+        'location',
+        'price'
+    ];
+
+    protected $casts = [
+        'price' => 'int'
+    ];
+}
